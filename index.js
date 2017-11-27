77ff9aa7e02dcce40c6dfbc08c9301a3dae5101f
Alexandr Bezobrazov <alexandr.bezobrazov@gmail.com>
2017-11-27 23:33:02 +0200

    Added mongoose model

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-require('./services/passport');
 const authRoutes = require('./routes/authRoutes');
+const keys = require('./config/keys');
+require('./services/passport');
+require('./models/User');
 
-const app = express();
+const app = express(keys.mongoURI);
 authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
