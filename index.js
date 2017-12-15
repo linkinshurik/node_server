5852cb54e113e765d4fd853fee6046e442bf1046
Alexandr Bezobrazov <alexandr.bezobrazov@gmail.com>
2017-12-15 17:28:21 +0200

    add some test posts

--- index.js
+++ index.js
@@ -7,7 +7,35 @@ require('./services/passport');
 
 mongoose.connect(keys.mongoURI, { useMongoClient: true });
 const app = express();
+let count = 0;
+let posts = [];
 authRoutes(app);
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.post('/posts', function (req, res) {
+    count++;
+    posts.push(req.body);
+    res.send({
+        posts: posts,
+        count: count
+    });
+});
+
+app.get('/posts', function (req, res) {
+    res.send({
+        posts: posts,
+        count: count
+    });
+});
+
+app.delete('/posts', function (req, res) {
+    posts = [];
+    count = 0;
+    res.send('ok');
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
