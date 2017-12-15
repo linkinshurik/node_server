eb6217c1cc5c4ace7aae577e57f146ad95a869d5
Alexandr Bezobrazov <alexandr.bezobrazov@gmail.com>
2017-12-15 17:37:17 +0200

    comment everything else

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const authRoutes = require('./routes/authRoutes');
-const keys = require('./config/keys');
-const mongoose = require('mongoose');
-require('./models/User');
-require('./services/passport');
+//const authRoutes = require('./routes/authRoutes');
+//const keys = require('./config/keys');
+//const mongoose = require('mongoose');
+//require('./models/User');
+//require('./services/passport');
 
-mongoose.connect(keys.mongoURI, { useMongoClient: true });
+//mongoose.connect(keys.mongoURI, { useMongoClient: true });
 const app = express();
 let count = 0;
 let posts = [];
-authRoutes(app);
+//authRoutes(app);
 
 app.use(express.urlencoded());
 app.use(express.json());
